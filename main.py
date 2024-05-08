19590706534a1b062971a4dd938d1d433a604cd3
amilyaad <145574574+amilyaad@users.noreply.github.com>
2024-05-08 09:27:11 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pygame
-# from tilemap import make_tile_map
 
 pygame.init()
-pygame.font.init()
-my_font = pygame.font.SysFont('Arial', 15)
-pygame.display.set_caption("Shoot the Fruit!")
 
 # set up variables for the display
-SCREEN_HEIGHT = 370
-SCREEN_WIDTH = 530
-size = (SCREEN_WIDTH, SCREEN_HEIGHT)
-screen = pygame.display.set_mode(size)
+SCREEN_HEIGHT = 640
+SCREEN_WIDTH = 1100
+LOWER_MARGIN = 50
+screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT + LOWER_MARGIN)
 
-file1 = 'tile1.png'
-image = pygame.image.load(file1)
-rect1 = image.get_rect()
+BG_ONE = pygame.image.load('').convert_alpha()
+BG_TWO = pygame.image.load('').convert_alpha()
+BG_THREE = pygame.image.load('').convert_alpha()
+BG_FOUR = pygame.image.load('').convert_alpha()
+
+def draw_bg(# inputted bg by user):
+    if
+    screen.blit(BG_ONE, (0,0 - LOWER_MARGIN))
+
+# -------- Main Program Loop -----------
+while run:
+
+    draw_bg()
+
+    # --- Main event loop
+    ## ----- NO BLIT ZONE START ----- ##
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+    pygame.display.update()
+    ##  ----- NO BLIT ZONE END  ----- ##
+
+
 
 screen.fill((0, 0, 0))
 screen.blit(image, rect1)
