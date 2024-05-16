053507a3c412f30d5b0bdec83fea083d0beb9b2e
amilyaad <145574574+amilyaad@users.noreply.github.com>
2024-05-16 09:27:21 -0400

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -1,34 +1,12 @@
 import pygame
 class Dashboard:
 
-   def __init__(self, x, y):
+   def __init__(self, image, x, y):
        self.x = x
        self.y = y
-       self.image = pygame.image.load("playbutton.png")
+       self.image = pygame.image.load(image)
        self.image_size = self.image.get_size()
        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
-   def __init__(self, x, y):
-       self.x = x
-       self.y = y
-       self.image = pygame.image.load("onebutton.png")
-       self.image_size = self.image.get_size()
-       self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
-   def __init__(self, x, y):
-       self.x = x
-       self.y = y
-       self.image = pygame.image.load("twobutton.png")
-       self.image_size = self.image.get_size()
-       self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
-   def __init__(self, x, y):
-       self.x = x
-       self.y = y
-       self.image = pygame.image.load("threebutton.png")
-       self.image_size = self.image.get_size()
-       self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
 
 
 
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ self.image = pygame.image.load("playbutton.png")
 self.image = pygame.image.load("onebutton.png")
 self.image = pygame.image.load("twobutton.png")
 self.image = pygame.image.load("threebutton.png")
+self.image = pygame.image.load("fourbutton.png")
 self.image_size = self.image.get_size()
 
 
@@ -30,8 +31,17 @@ BG_FOUR = pygame.image.load('').convert_alpha()
 
 chosen_bg = 0
 
+not_play = True
+dashboard = False
+
+button_play = Dashboard("playbutton.png", 40, 40)
+button_one = Dashboard("onebutton.png", 40, 40)
+button_two = Dashboard("twobutton.png", 40, 40)
+button_three = Dashboard("threebutton.png", 40, 40)
+button_four = Dashboard("fourbutton.png", 40, 40)
+
 def draw_bg(chosen_bg):
-    if chosen_bg == 1
+    if chosen_bg == 1:
         screen.blit(BG_ONE, (0,0 - LOWER_MARGIN))
     elif chosen_bg == 2:
         screen.blit(BG_TWO, (0, 0 - LOWER_MARGIN))
@@ -39,9 +49,8 @@ def draw_bg(chosen_bg):
         screen.blit(BG_THREE, (0, 0 - LOWER_MARGIN))
     elif chosen_bg == 4:
         screen.blit(BG_FOUR, (0, 0 - LOWER_MARGIN))
+    dashboard = False
 
-not_play = True
-dashboard = False
 # -------- Main Program Loop -----------
 while run:
 
@@ -51,9 +60,22 @@ while run:
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
         elif not_play:
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP and button_play.rect.collidepoint(event.pos):
                 not_play = False
                 dashboard = True
+            if dashboard:
+                if pygame.MOUSEBUTTONUP and one_button.rect.collidepoint(event.pos):
+                    bg = 1
+                    draw_bg(bg)
+                if pygame.MOUSEBUTTONUP and two_button.rect.collidepoint(event.pos):
+                    bg = 2
+                    draw_bg(bg)
+                if pygame.MOUSEBUTTONUP and three_button.rect.collidepoint(event.pos):
+                    bg = 3
+                    draw_bg(bg)
+                if pygame.MOUSEBUTTONUP and four_button.rect.collidepoint(event.pos):
+                    bg = 4
+                    draw_bg(bg)
 
     pygame.display.update()
     ##  ----- NO BLIT ZONE END  ----- ##
@@ -64,8 +86,12 @@ screen.fill(('background.png'))
 if not_play:
     screen.blit(display_title, (0,0))
     screen.blit(display_message, (0, 0))
-    screen.blit(self.play_botton, (0,0))
+    screen.blit(button_play.image, button_play.rect)
 if dashboard:
+    screen.blit(button_one.image, button_one.rect)
+    screen.blit(button_two.image, button_two.rect)
+    screen.blit(button_three.image, button_three.rect)
+    screen.blit(button_four.image, button_four.rect)
 
 
 pygame.display.update()
