230a0d2df50f5b567769c34e44e1e254066f85c9
amilyaad <145574574+amilyaad@users.noreply.github.com>
2024-05-31 09:26:16 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pygame
+import button
+
+from dashboard import Dashboard
+from tilemap import Tile
+from button import Button
+from player import Sprite_One
+from player import Sprite_Two
 
 pygame.init()
 pygame.font.init()
@@ -9,21 +16,23 @@ message = "Welcome to Castle Defender! Click play to being!"
 display_title = my_font.render(message, True, (255, 255, 255))
 display_message = my_font.render(message, True, (255, 255, 255))
 
-self.image = pygame.image.load("playbutton.png")
-self.image = pygame.image.load("onebutton.png")
-self.image = pygame.image.load("twobutton.png")
-self.image = pygame.image.load("threebutton.png")
-self.image = pygame.image.load("fourbutton.png")
-self.image_size = self.image.get_size()
+b_play = pygame.image.load("playbutton.png")
+b_one = pygame.image.load("onebutton.png")
+b_two = pygame.image.load("twobutton.png")
+b_three = pygame.image.load("threebutton.png")
+b_four = pygame.image.load("fourbutton.png")
+p_one = pygame.image.load("spriteone.png")
+p_two = pygame.image.load("spritetwo.png")
 
 
 # set up variables for the display
 SCREEN_HEIGHT = 640
 SCREEN_WIDTH = 1000
 LOWER_MARGIN = 50
+SIDE_MARGIN = 300
 screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT + LOWER_MARGIN)
 ROWS = 16
-COLS = 25
+MAX_COLS = 25
 TILE_SIZE = SCREEN_HEIGHT // ROWS
 TILE_TYPES = 7
 current_tile = 0
@@ -66,7 +75,7 @@ button_four = Dashboard("fourbutton.png", 40, 40)
 
 def draw_bg(chosen_bg):
     if chosen_bg == 1:
-        screen.blit(BG_ONE, (0,0 - LOWER_MARGIN))
+        screen.blit(BG_ONE, (0, 0 - LOWER_MARGIN))
     elif chosen_bg == 2:
         screen.blit(BG_TWO, (0, 0 - LOWER_MARGIN))
     elif chosen_bg == 3:
@@ -78,16 +87,16 @@ def draw_bg(chosen_bg):
 def draw_grid():
 	#vertical lines
 	for c in range(MAX_COLS + 1):
-		pygame.draw.line(screen, WHITE, (c * TILE_SIZE - scroll, 0), (c * TILE_SIZE - scroll, SCREEN_HEIGHT))
+		pygame.draw.line(screen, (255, 255, 255), (c * TILE_SIZE, 0), (c * TILE_SIZE, SCREEN_HEIGHT))
 	#horizontal lines
 	for c in range(ROWS + 1):
-		pygame.draw.line(screen, WHITE, (0, c * TILE_SIZE), (SCREEN_WIDTH, c * TILE_SIZE))
+		pygame.draw.line(screen, (255, 255, 255), (0, c * TILE_SIZE), (SCREEN_WIDTH, c * TILE_SIZE))
 
 def draw_world():
 	for y, row in enumerate(world_data):
 		for x, tile in enumerate(row):
 			if tile >= 0:
-				screen.blit(img_list[tile], (x * TILE_SIZE - scroll, y * TILE_SIZE))
+				screen.blit(img_list[tile], (x * TILE_SIZE, y * TILE_SIZE))
 
 save_button = button.Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT + LOWER_MARGIN - 50, save_img, 1)
 load_button = button.Button(SCREEN_WIDTH // 2 + 200, SCREEN_HEIGHT + LOWER_MARGIN - 50, load_img, 1)
@@ -106,7 +115,6 @@ for i in range(len(img_list)):
 
 # -------- Main Program Loop -----------
 while run:
-
     while not not_play:
         if keys[pygame.K_RIGHT]:
             p_one.move_direction("right")
@@ -126,8 +134,7 @@ while run:
         elif keys[pygame.K_s]:
             p_two.move_direction("down")
 
-    # --- Main event loop
-    ## ----- NO BLIT ZONE START ----- ##
+# --- Main event loop --- #
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
@@ -135,38 +142,38 @@ while run:
             if event.type == pygame.MOUSEBUTTONUP and button_play.rect.collidepoint(event.pos):
                 dashboard = True
             if dashboard:
-                if pygame.MOUSEBUTTONUP and one_button.rect.collidepoint(event.pos):
+                if pygame.MOUSEBUTTONUP and b_one.rect.collidepoint(event.pos):
                     bg = 1
                     draw_bg(bg)
                     not_play = False
                     lvl_one = True
-                if pygame.MOUSEBUTTONUP and two_button.rect.collidepoint(event.pos):
+                if pygame.MOUSEBUTTONUP and b_two.rect.collidepoint(event.pos):
                     bg = 2
                     draw_bg(bg)
                     not_play = False
                     lvl_two = True
-                if pygame.MOUSEBUTTONUP and three_button.rect.collidepoint(event.pos):
+                if pygame.MOUSEBUTTONUP and b_three.rect.collidepoint(event.pos):
                     bg = 3
                     draw_bg(bg)
                     not_play = False
                     lvl_level = False
-                if pygame.MOUSEBUTTONUP and four_button.rect.collidepoint(event.pos):
+                if pygame.MOUSEBUTTONUP and b_four.rect.collidepoint(event.pos):
                     bg = 4
                     draw_bg(bg)
                     create_map = True
                     not_play = False
-        elif not(not_play) and create_map:
-            pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
+        elif not not_play and create_map:
+            pygame.draw.rect(screen, (0, 255, 0), (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
 
             button_count = 0
             for button_count, i in enumerate(button_list):
                 if i.draw(screen):
                     current_tile = button_count
 
-            pygame.draw.rect(screen, RED, button_list[current_tile].rect, 3)
+            pygame.draw.rect(screen, (255, 0, 0), button_list[current_tile].rect, 3)
 
             pos = pygame.mouse.get_pos()
-            x = (pos[0] + scroll) // TILE_SIZE
+            x = (pos[0]) // TILE_SIZE
             y = pos[1] // TILE_SIZE
 
             # check that the coordinates are within the tile area
@@ -179,11 +186,10 @@ while run:
                     world_data[y][x] = -1
 
     pygame.display.update()
-    ##  ----- NO BLIT ZONE END  ----- ##
 
 if not_play:
-    screen.fill(('background.png'))
-    screen.blit(display_title, (0,0))
+    screen.fill('background.png')
+    screen.blit(display_title, (0, 0))
     screen.blit(display_message, (0, 0))
     screen.blit(button_play.image, button_play.rect)
 if dashboard:
@@ -192,10 +198,10 @@ if dashboard:
     screen.blit(button_three.image, button_three.rect)
     screen.blit(button_four.image, button_four.rect)
 if lvl_one:
-    screen.fill(('background.png'))
+    screen.fill('background.png')
 if lvl_two:
-    screen.fill(('background.png'))
+    screen.fill('background.png')
 if lvl_three:
-    screen.fill(('background.png'))
+    screen.fill('background.png')
 
 pygame.display.update()
