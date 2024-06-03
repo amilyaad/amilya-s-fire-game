e95a978f7eba834fa289ddf4818229243743b5c8
amilyaad <145574574+amilyaad@users.noreply.github.com>
2024-06-03 09:30:03 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -185,6 +185,30 @@ while run:
                 if pygame.mouse.get_pressed()[2] == 1:
                     world_data[y][x] = -1
 
+            if save_button.draw(screen):
+                # save level data
+                with open(f'level{level}_data.csv', 'w', newline='') as csvfile:
+                    writer = csv.writer(csvfile, delimiter=',')
+                    for row in world_data:
+                        writer.writerow(row)
+                # alternative pickle method
+                # pickle_out = open(f'level{level}_data', 'wb')
+                # pickle.dump(world_data, pickle_out)
+                # pickle_out.close()
+            if load_button.draw(screen):
+                # load in level data
+                # reset scroll back to the start of the level
+                scroll = 0
+                with open(f'level{level}_data.csv', newline='') as csvfile:
+                    reader = csv.reader(csvfile, delimiter=',')
+                    for x, row in enumerate(reader):
+                        for y, tile in enumerate(row):
+                            world_data[x][y] = int(tile)
+            # alternative pickle method
+            # world_data = []
+            # pickle_in = open(f'level{level}_data', 'rb')
+            # world_data = pickle.load(pickle_in)
+
     pygame.display.update()
 
 if not_play:
